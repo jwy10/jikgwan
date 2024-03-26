8b06399b11d8ac55d4eb24682af5f6e57b456a7d
jwy10 <1998dnjsdud@gmail.com>
2024-03-26 13:59:27 +0900

    0326-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import IntroHeader from './components/IntroHeader';
 
 function App() {
   return (
     <div className="App">
-      <h1>직관</h1>
+      <h1>JikGwan</h1>
+      <IntroHeader />
     </div>
   );
 }
---
+++ src/components/IntroHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function IntroHeader() {
+  return (
+    <div>
+      <h3>국내 직관</h3>
+    </div>
+  )
+}
