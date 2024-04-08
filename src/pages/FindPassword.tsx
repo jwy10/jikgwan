b9ccefc33e89eb392ff6e72e15331873eca727dd
jwy10 <1998dnjsdud@gmail.com>
2024-04-09 01:36:26 +0900

    0409 로그인, 회원가입, 비번찾기 업데이트

---
+++ src/pages/FindPassword.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../CSS/Member.css';
+
+export default function FindPassword(): JSX.Element {
+  // const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  // const togglePasswordVisibility = (): void => {
+  //   setShowPassword(!showPassword);
+  // };
+
+  const hide: string = '/hide.png';
+  const show: string = '/show.png';
+
+  const handleEmailClear = (): void => {
+    setEmail('');
+  };
+
+  const handleClick = (): void => {
+    setClicked(true);
+  };
+
+  return (
+    <>
+      <div className="Login">
+        <div className="findPasswordBox">
+          <Link to={'/home'}>
+            <p style={{ fontSize: '36px', margin: '30px 0px' }}>JikGwan</p>
+          </Link>
+          <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>
+            비밀번호를 잊으셨습니까?
+          </h2>
+          <div className="noneSignUp">
+            <div className="pwNotice">
+              <h5 style={{ marginRight: '5px' }}>
+                귀하의 계정과 연결된 이메일 주소를 입력하시면
+              </h5>
+              <h5 style={{ marginBottom: '20px', marginRight: '5px' }}>
+                비밀번호를 재설정할 수 있는 링크를 보내드립니다.
+              </h5>
+            </div>
+          </div>
+          <div className="findInfo">
+            <div className="findEmail">
+              <input
+                className="findInput"
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ marginLeft: '10px' }}
+              />
+              <button className="emailBtn" onClick={handleEmailClear}>
+                <p style={{ fontSize: '20px' }}>x</p>
+              </button>
+            </div>
+            <button
+              className="sendEmailBtn"
+              onClick={handleClick}
+              style={{
+                backgroundColor: clicked ? 'gray' : 'rgb(0, 132, 255)',
+                color: 'white',
+              }}
+            >
+              이메일 보내기
+            </button>
+          </div>
+          <Link to={'/login'}>
+            <h5 style={{ color: 'blue', marginBottom: '30px' }}>
+              {' '}
+              로그인으로 돌아가기
+            </h5>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import '../CSS/Login.css';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function SignUp() {
+import '../CSS/Member.css';
+
+export default function SignUp(): JSX.Element {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [clicked, setClicked] = useState<boolean>(false); // 클릭 상태를 추적하는 상태 추가
+
+  const togglePasswordVisibility = (): void => {
+    setShowPassword(!showPassword);
+  };
+
+  const hide: string = '/hide.png';
+  const show: string = '/show.png';
+
+  const handleEmailClear = (): void => {
+    setEmail('');
+  };
+
+  const handleClick = (): void => {
+    setClicked(true); // 클릭되었음을 표시
+  };
+
   return (
     <>
       <div className="Login">
@@ -12,29 +33,73 @@ export default function SignUp() {
           </Link>
           <h2 style={{ fontSize: '28px', marginBottom: '15px' }}>회원가입</h2>
           <div className="noneSignUp">
-            <h6 style={{ marginBottom: '15px' }}>이미 계정이 있으신가요?</h6>
+            <h5 style={{ marginBottom: '15px', marginRight: '5px' }}>
+              이미 가입되어 있으신가요?
+            </h5>
             <Link to={'/login'}>
-              <h6 style={{ color: 'blue' }}>로그인</h6>
+              <h5 style={{ color: 'blue' }}> 로그인</h5>
             </Link>
           </div>
           <div className="loginInfo">
             <div className="loginEmail">
               <input
                 className="loginInput"
-                type="text"
+                type="email"
                 placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 style={{ marginLeft: '10px' }}
               />
-              <button className="emailBtn">x</button>
+              <button className="emailBtn" onClick={handleEmailClear}>
+                <p style={{ fontSize: '20px' }}>x</p>
+              </button>
+            </div>
+
+            <div className="loginEmail">
+              <input
+                className="loginInput"
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ marginLeft: '10px' }}
+              />
+              <button className="emailBtn" onClick={handleEmailClear}>
+                <p style={{ fontSize: '20px' }}>x</p>
+              </button>
             </div>
             <div className="loginPassword">
               <input
                 className="loginInput"
-                type="text"
+                type={showPassword ? 'text' : 'password'}
                 placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 style={{ marginLeft: '10px' }}
               />
-              <button className="passwordBtn">x</button>
+              <button
+                className="passwordBtn"
+                onClick={togglePasswordVisibility}
+              >
+                <img
+                  src={showPassword ? hide : show}
+                  alt="password visibility"
+                  style={{ width: '20px', height: '20px' }}
+                />
+              </button>
+            </div>
+            <div className="loginEmail">
+              <input
+                className="loginInput"
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ marginLeft: '10px' }}
+              />
+              <button className="emailBtn" onClick={handleEmailClear}>
+                <p style={{ fontSize: '20px' }}>x</p>
+              </button>
             </div>
           </div>
         </div>
